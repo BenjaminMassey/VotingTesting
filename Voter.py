c62fa9b8b26c2ec80db7796f701f0ca421399790
BenjaminMassey <benjamin.w.massey@gmail.com>
2021-02-02 20:18:20 -0800

    Fully setup placeholder layout

--- Voter.py
+++ Voter.py
@@ -5,7 +5,7 @@ from cryptography.fernet import Fernet
 
 # The following encryption functions are from here:
 # https://devqa.io/encrypt-decrypt-data-python/
-
+# They get the Fernet encryption ready
 def generate_key():
     key = Fernet.generate_key()
     with open("secret.key", "wb") as key_file:
@@ -31,13 +31,13 @@ generate_key()
 
 # GUI Setup
 window = Tk()
-
 window.title("Voting Machine")
-window.geometry('1280x720')
+window.geometry('1150x600')
 
+## Here is the top section, used to vote
 # Title label
 infoLabel = Label(window, text="President of the United States")
-infoLabel.config(font=("Courier", 16))
+infoLabel.config(font=("Courier", 16, "bold"))
 infoLabel.grid(row=0, column=1)
 
 # Number value corresponding to the vote
@@ -82,62 +82,156 @@ def process():
         result = getCandidate(radioValue.get())
         voteText.set(result)
 
-#Label(window,text="").pack() # Spacing
-
 submitButton = Button(window, text="Submit", command=process)
 submitButton.config(font=("Courier", 12))
 submitButton.grid(row=i+1, column=1)
+
+Label(window,text="").grid(row=i+2, column=1)
+
+resultLabel = Label(window, text="Result:")
+resultLabel.config(font=("Courier", 12))
+resultLabel.grid(row=i+3, column=1)
 
 voteText = StringVar()
 voteText.set("---")
 voteLabel = Label(window, textvariable=voteText)
 voteLabel.config(font=("Courier", 16))
-voteLabel.grid(row=i+2, column=0, padx=175)
+voteLabel.grid(row=i+4, column=1, padx=100)
 
-#Label(window,text="").pack() # Spacing
+Label(window,text="").grid(row=i+5, column=1)
+
+
+## End of top section, now the left encryption version
+firstSectionLabel = Label(window, text="Caesar Cipher")
+firstSectionLabel.config(font=("Courier", 16, "bold"))
+firstSectionLabel.grid(row=i+6, column=0)
+
+caesar_result = None
+
+def encrypt1():
+    global voteText, encryptText1, caesar_result
+    votedCandidate = voteText.get()
+    if votedCandidate == "Please vote :)" or votedCandidate == "---":
+        return
+    encryptText1.set("Place")
+    caesar_result = "Place"
+
+encryptButton1 = Button(window, text="Encrypt", command=encrypt1)
+encryptButton1.config(font=("Courier", 12))
+encryptButton1.grid(row=i+7, column=0, padx=100)
+
+encryptText1 = StringVar()
+encryptText1.set("---")
+encryptLabel1 = Label(window, textvariable=encryptText1, wraplength=300)
+encryptLabel1.config(font=("Courier", 16))
+encryptLabel1.grid(row=i+8, column=0, padx=100)
+
+def decrypt1():
+    global decryptText1, caesar_result
+    if caesar_result == None:
+        return
+    #caesar_result
+    decryptText1.set("Holder")
+
+decryptButton1 = Button(window, text="Decrypt", command=decrypt1)
+decryptButton1.config(font=("Courier", 12))
+decryptButton1.grid(row=i+9, column=0, padx=100)
+
+decryptText1 = StringVar()
+decryptText1.set("---")
+decryptLabel1 = Label(window, textvariable=decryptText1)
+decryptLabel1.config(font=("Courier", 16))
+decryptLabel1.grid(row=i+10, column=0, padx=100)
+
+
+## End of the left encyrption, now the middle encryption version
+secondSectionLabel = Label(window, text="Fernet Encryption")
+secondSectionLabel.config(font=("Courier", 16, "bold"))
+secondSectionLabel.grid(row=i+6, column=1)
 
 # Here we will store our encrypted bytestring
 # Tried to avoid this, but encoding got weird
-encrypted_result = None
+fernet_result = None
 
 # Look at our voted candidate, then encrypt it
-def encrypt():
-    global voteText, encryptText, encrypted_result
+def encrypt2():
+    global voteText, encryptText2, fernet_result
     votedCandidate = voteText.get()
     if votedCandidate == "Please vote :)" or votedCandidate == "---":
         return
     encryptedCandidate = encrypt_message(votedCandidate)
-    encryptText.set(encryptedCandidate)
-    encrypted_result = encryptedCandidate
+    encryptText2.set(encryptedCandidate)
+    fernet_result = encryptedCandidate
 
-encryptButton = Button(window, text="Encrypt", command=encrypt)
-encryptButton.config(font=("Courier", 12))
-encryptButton.grid(row=i+3, column=0, padx=175)
+encryptButton2 = Button(window, text="Encrypt", command=encrypt2)
+encryptButton2.config(font=("Courier", 12))
+encryptButton2.grid(row=i+7, column=1, padx=100)
 
-encryptText = StringVar()
-encryptText.set("---")
-encryptLabel = Label(window, textvariable=encryptText, wraplength=300)
-encryptLabel.config(font=("Courier", 16))
-encryptLabel.grid(row=i+4, column=0, padx=175)
-
-#Label(window,text="").pack() # Spacing
+encryptText2 = StringVar()
+encryptText2.set("-"*100)
+encryptLabel2 = Label(window, textvariable=encryptText2, wraplength=300)
+encryptLabel2.config(font=("Courier", 16))
+encryptLabel2.grid(row=i+8, column=1, padx=100)
 
 # Look at our encrypted candidate variable and decrypt it
-def decrypt():
-    global decryptText, encrypted_result
-    if encrypted_result == None:
+def decrypt2():
+    global decryptText2, fernet_result
+    if fernet_result == None:
         return
-    decryptedCandidate = decrypt_message(encrypted_result)
-    decryptText.set(decryptedCandidate)
+    decryptedCandidate = decrypt_message(fernet_result)
+    decryptText2.set(decryptedCandidate)
 
-decryptButton = Button(window, text="Decrypt", command=decrypt)
-decryptButton.config(font=("Courier", 12))
-decryptButton.grid(row=i+5, column=0, padx=175)
+decryptButton2 = Button(window, text="Decrypt", command=decrypt2)
+decryptButton2.config(font=("Courier", 12))
+decryptButton2.grid(row=i+9, column=1, padx=100)
 
-decryptText = StringVar()
-decryptText.set("---")
-decryptLabel = Label(window, textvariable=decryptText)
-decryptLabel.config(font=("Courier", 16))
-decryptLabel.grid(row=i+6, column=0, padx=175)
+decryptText2 = StringVar()
+decryptText2.set("---")
+decryptLabel2 = Label(window, textvariable=decryptText2)
+decryptLabel2.config(font=("Courier", 16))
+decryptLabel2.grid(row=i+10, column=1, padx=100)
+
+
+## End of middle encryption, now the right encryption version
+thirdSectionLabel = Label(window, text="Bestest Coolest Version")
+thirdSectionLabel.config(font=("Courier", 16, "bold"))
+thirdSectionLabel.grid(row=i+6, column=2)
+
+mystery_result = None
+
+def encrypt3():
+    global voteText, encryptText3, caesar_result
+    votedCandidate = voteText.get()
+    if votedCandidate == "Please vote :)" or votedCandidate == "---":
+        return
+    encryptText3.set("Place")
+    caesar_result = "Place"
+
+encryptButton3 = Button(window, text="Encrypt", command=encrypt3)
+encryptButton3.config(font=("Courier", 12))
+encryptButton3.grid(row=i+7, column=2, padx=100)
+
+encryptText3 = StringVar()
+encryptText3.set("---")
+encryptLabel3 = Label(window, textvariable=encryptText3, wraplength=300)
+encryptLabel3.config(font=("Courier", 16))
+encryptLabel3.grid(row=i+8, column=2, padx=100)
+
+def decrypt3():
+    global decryptText3, mystery_result
+    if caesar_result == None:
+        return
+    #caesar_result
+    decryptText3.set("Holder")
+
+decryptButton3 = Button(window, text="Decrypt", command=decrypt3)
+decryptButton3.config(font=("Courier", 12))
+decryptButton3.grid(row=i+9, column=2, padx=100)
+
+decryptText3 = StringVar()
+decryptText3.set("---")
+decryptLabel3 = Label(window, textvariable=decryptText3)
+decryptLabel3.config(font=("Courier", 16))
+decryptLabel3.grid(row=i+10, column=2, padx=100)
 
 window.mainloop()
